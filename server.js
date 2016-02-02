acccb241a091c03e5f0ee34a671ac333b98bebf9
Chamira Fernando <chamira.fernando@makingwaves.com>
2016-02-02 13:42:08 +0100

    test 5

--- server.js
+++ server.js
@@ -2,9 +2,23 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write("This is before end\n");
-  res.end('Hello World\n');
-  res.write("After end");
+
+	route(req,res);
+
 }).listen(port);
+
+function readGoogle(request,response) {
+
+	//var lang = require("./gen_lang.js");
+
+}
+
+
+function route(request,response) {
+
+	response.writeHead(200, { 'Content-Type': 'text/plain' });
+  	response.write("This is before end\n");
+  	response.end(request.url);
+
+}
 return;
