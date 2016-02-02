2193113257d747a38d46a5a360bb75722129c725
Chamira Fernando <chamira.fernando@makingwaves.com>
2016-02-02 12:13:21 +0100

    test 3

--- gen_lang.js
+++ gen_lang.js
@@ -1,12 +1,4 @@
 
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World azure\n');
-}).listen(port);
-return;
-
 var argvs = process.argv.slice(2);
 console.dir(argvs);
 if (argvs.length != 3) {
