ba9f54ac4d3eb97b51e346cd2a057acf71e4e700
Chamira Fernando <chamira.fernando@makingwaves.com>
2016-02-02 13:17:58 +0100

    test 4

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World.. test\n');
+  res.write("This is before end\n");
+  res.end('Hello World\n');
+  res.write("After end");
 }).listen(port);
 return;
